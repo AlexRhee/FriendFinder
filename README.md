6c868911bdb98823c9656d5a5f33a027379e3662
arhee <arheeee@gmail.com>
2018-03-22 23:01:48 -0700

    v0.5 not posting correctly yet, score calculator logic not created

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # FriendFinder
+
+I couldn't figure out how to properly send the array information through the post, it kept coming up with empty values.
+
+The next step would be to run the user scores array through the for loop with the scores from existing friends and then get the difference between each score (absolute value) and then add all those scores up and save them as variables then the lowest score will be selected as a friend for the user and will be displayed in a div that will show up on the survey page.
---
+++ data/data/friends.js
@@ -0,0 +1,165 @@
+
+var friendList =[ {
+    name: "tobias",
+    photo: "",
+    scores: [
+    1,
+    2,
+    3,
+    4,
+    5,
+    1,
+    2,
+    3,
+    4,
+    5
+]
+},
+{
+    name: "boban",
+    photo: "",
+    scores: [
+   2,
+   3,
+   4,
+   2,
+   2,
+   5,
+   1,
+   5,
+   2,
+   3,
+]
+},
+{
+    name: "pat",
+    photo: "",
+    scores: [
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+ ]
+},
+{
+     name: "montrezl",
+     photo: "",    
+    scores:[
+    3,
+    3,
+    3,
+    3,
+    3,
+    2,
+    2,
+    2,
+    2,
+    2,
+ ]
+},
+{
+     name: "deandre",
+    photo: "",
+    scores: [
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+ ]
+},
+{
+    name: "milos",
+   photo: "",
+   scores: [
+   5,
+   5,
+   5,
+   5,
+   5,
+   5,
+   5,
+   5,
+   5,
+   5,
+]
+},
+{
+    name: "austin",
+   photo: "",
+   scores: [
+   5,
+   5,
+   5,
+   5,
+   5,
+   5,
+   5,
+   5,
+   5,
+   5,
+]
+},
+{
+    name: "jawun",
+   photo: "",
+   scores: [
+   5,
+   5,
+   5,
+   5,
+   5,
+   5,
+   5,
+   5,
+   5,
+   5,
+]
+},
+{
+    name: "sindarius",
+   photo: "",
+   scores: [
+   5,
+   5,
+   5,
+   5,
+   5,
+   5,
+   5,
+   5,
+   5,
+   5,
+]
+},
+
+ {
+    name: "sam",
+   photo: "",
+   scores: [
+   5,
+   5,
+   5,
+   5,
+   5,
+   5,
+   5,
+   5,
+   5,
+   5,
+]
+}
+];
+
+ module.exports = friendList;
